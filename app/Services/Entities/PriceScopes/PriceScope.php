d4cd5a7f84ebe4a55f1f03e19fb18346fefe9cfb
chelsymooy <chelsymooy1108@gmail.com>
2016-09-01 13:53:18 +0700

    fixed price scopes

--- app/Services/Entities/PriceScopes/PriceScope.php
+++ app/Services/Entities/PriceScopes/PriceScope.php
@@ -29,6 +29,7 @@ class PriceScope implements ScopeInterface
 		->selectraw('IFNULL(prices.price, 0) as price')
 		->selectraw('IFNULL(prices.promo_price, 0) as promo_price')
 		->selectraw('IFNULL(prices.started_at, NOW()) as price_start')
+		->selectraw('IFNULL(prices2.started_at, NOW()) as price_end')
 		->leftjoin('prices', function ($join)
 		 {
             $join->on ( 'prices.product_id', '=', 'products.id' )
@@ -36,7 +37,16 @@ class PriceScope implements ScopeInterface
             ->where('prices.started_at', '<=', date('Y-m-d H:i:s'))
             ->wherenull('prices.deleted_at')
             ;
-		});
+		})
+		->leftjoin(DB::raw('prices as prices2'), function ($join)
+		 {
+            $join->on ( 'prices2.product_id', '=', 'products.id' )
+			->on(DB::raw('(prices2.id = (select id from prices as tl3 where tl3.product_id = prices2.product_id and tl3.deleted_at is null and tl3.started_at <= "'.date('Y-m-d H:i:s').'" order by tl3.started_at desc limit 1 offset 2))'), DB::raw(''), DB::raw(''))
+            ->where('prices2.started_at', '<=', date('Y-m-d H:i:s'))
+            ->wherenull('prices2.deleted_at')
+            ;
+		})
+		;
 	}
 
 	/**
